90a413a1d5d417c1babe97ffe83f02a59e7c1a11
unknown <jvsantos2003@hotmail.com>
2023-04-26 14:47:21 -0300

    1ª versão - Tela da biblioteca

---
+++ Tela-Biblioteca/src/main/java/br/com/joao/tela/biblioteca/TelaBiblioteca.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.joao.tela.biblioteca;
+
+/**
+ *
+ * @author João
+ */
+public class TelaBiblioteca {
+
+    public static void main(String[] args) {
+        loginProvisórioFrame login = new loginProvisórioFrame();
+        login.setVisible(true);
+    }
+}
